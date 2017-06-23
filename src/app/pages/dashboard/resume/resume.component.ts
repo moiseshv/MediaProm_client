d629f8fdbf3075ac87b3e86351d5c1bbbe332d7d
moises <moiseshv@gmail.com>
2017-06-22 23:34:52 -0400

    Resumen aun malo con el tema del filtro por usuari

--- src/app/pages/dashboard/resume/resume.component.ts
+++ src/app/pages/dashboard/resume/resume.component.ts
@@ -1,5 +1,6 @@
-import {Component} from '@angular/core';
-import {ResumeService} from './resume.service';
+import { Component } from '@angular/core';
+import { ResumeService } from './resume.service';
+import {GlobalState} from '../../../global.state';
 
 
 @Component({
@@ -10,21 +11,50 @@ import {ResumeService} from './resume.service';
 
 export class Resume {
 
-  public data: Object;
+  public data = [{},{},{},{}];
   private _init = false;
+  currentUser: Object;
+  currentUserID: string;
 
-  constructor(private _resumeService: ResumeService) {
-    this.data = this._resumeService.getData();
+  //constructor(private _resumeService: ResumeService) {
+   constructor(private _state:GlobalState, private _resumeService: ResumeService) {
+    this._state.subscribe('user.current', (user) => {
+       this.currentUser = user;
+       console.log('usuario llego')
+       console.log(user);
+       console.log( user['objectId']); 
+       this.currentUserID = (user != undefined ) ? user['objectId'] : undefined;
+       console.log(this.currentUserID);
+       this.data = _resumeService.getDummyData(); 
+     });
+   
   }
 
-  ngAfterViewInit() {
+  async ngAfterViewInit() {
+    console.log("init");
+    
     if (!this._init) {
-      this._updateData();
+      console.log(this.currentUserID);
+      try {
+         var st = await this._getResData(this.currentUserID);
+      console.log("respuesta1");
+      console.log(st);
+      if (st != undefined){
+        this.data=st;
+      }
+      } catch (error) {
+         console.log(error);
+      }
+     
       this._init = true;
     }
   }
 
-  private _updateData() {
-     this.data = this._resumeService.getData();
+  private async _getResData(userid : string) {
+    console.log('geting data');
+    console.log(userid);
+    var stats = await this._resumeService.getResumeData(userid);
+    return stats;
   }
+
 }
--- src/app/pages/dashboard/resume/resume.service.ts
+++ src/app/pages/dashboard/resume/resume.service.ts
@@ -1,47 +1,213 @@
-import {Injectable} from '@angular/core';
-import {BaThemeConfigProvider, colorHelper} from '../../../theme';
+import { Injectable } from '@angular/core';
+import { MEDIA_SERVER_URL, API_END } from './../../../app.configs';
 
 @Injectable()
 export class ResumeService {
+  private serverUrl = MEDIA_SERVER_URL + API_END;
+  public devices: Array<Object>;
+  public videos: Object;
+  public plans: Object;
 
-  constructor(private _baConfig:BaThemeConfigProvider) {
+  constructor() {
   }
 
-  getData() {
-    return [
-      {
+  public async getResumeData(userid: string) {
+    console.log("Resume Service...");
 
-        description: 'dashboard.devices',
-        stats: '127',
-        icon: 'fa fa-desktop fa-5x',
-      }, {
+   
+    var devicedata = {
+      description: 'dashboard.devices',
+      stats: -1,
+      icon: 'fa fa-desktop fa-5x',
+    }
 
-        description: 'dashboard.plans',
-        stats: '4',
-        icon: 'fa fa-money fa-5x',
-      }, {
+    var videodata = {
+      description: 'dashboard.videos',
+      stats: -1,
+      icon: 'fa fa-file-movie-o fa-5x',
+    }
 
-        description: 'dashboard.videos',
-        stats: '178',
-        icon: 'fa fa-file-movie-o fa-5x',
-      }, {
+    var plandata = {
+      description: 'dashboard.plans',
+      stats: -1,
+      icon: 'fa fa-money fa-5x',
+    }
 
-        description: 'dashboard.shared_devices',
-        stats: '23',
-        icon: 'fa fa-share-alt fa-5x',
+    var shareddata = {
+      description: 'dashboard.shared_devices',
+      stats: -1,
+      icon: 'fa fa-share-alt fa-5x',
+    }
+
+
+     try {
+      var udevices = await this.getDevicesbyUser(userid);
+      if (udevices == undefined) {
+        var message = "Can not get devices in this moment.";
+        console.log(message);
       }
-    ];
-    /*
+      else {
+        console.log(udevices);
+        var errorcode: string = udevices["code"];
+        if (errorcode != undefined) {
+          message = udevices["error"];
+          console.log(message);       }
+        else {
+          this.devices = udevices;                   
+          devicedata['stats'] = ((udevices != undefined) && (udevices.results != undefined))? Object.keys(udevices.results).length : -1;
+          
+        }
+      }
+    } catch (error) {
 
-    var stats = {
-                'devices'         : 10,  
-                'videos'          : 23,
-                'shared_devices'  : 34,
-                'plans'           : 3
-              }  
-              
-              */
+    }
+
+
+    try {
+      var uvideos = await this.getVideosbyUser(userid);
+      if (uvideos == undefined) {
+        var message = "Can not get videos in this moment.";
+        console.log(message);
+      }
+      else {
+        console.log(uvideos);
+        var errorcode: string = uvideos["code"];
+        if (errorcode != undefined) {
+          message = udevices["error"];
+          console.log(message);
+        }
+        else {
+          this.videos = uvideos;
+          videodata['stats'] = ((uvideos != undefined) && (uvideos.results != undefined))? Object.keys(uvideos.results).length : -1;
+        }
+      }
+    } catch (error) {
+
+    }
+
+    // Visualizar datos estaditicos
+    var returnObj = [devicedata, videodata, plandata, shareddata];
+
+    return returnObj;
+
 
   }
 
+
+  public getDummyData() {
+
+ 
+    var devicedata = {
+      description: 'dashboard.devices',
+      stats: 'NA',
+      icon: 'fa fa-desktop fa-5x',
+    }
+
+    var videodata = {
+      description: 'dashboard.videos',
+      stats: 'NA',
+      icon: 'fa fa-file-movie-o fa-5x',
+    }
+
+    var plandata = {
+      description: 'dashboard.plans',
+      stats: 'NA',
+      icon: 'fa fa-money fa-5x',
+    }
+
+    var shareddata = {
+      description: 'dashboard.shared_devices',
+      stats: 'NA',
+      icon: 'fa fa-share-alt fa-5x',
+    }
+     
+      // Visualizar datos estaditicos
+    return [devicedata,videodata,plandata,shareddata];  
+    
+
+  }
+
+
+
+  private async getDevicesbyUser(userid: string) {
+    //where={"owner":{"__type":"Pointer","className":"_User","objectId":"wJ1XDdGTFf"}}
+    userid = "wJ1XDdGTFf";
+    console.log("Devices request");
+    var requestobj = { "owner": { "__type": "Pointer", "className": "_User", "objectId": "wJ1XDdGTFf" }};
+    requestobj.owner.objectId = userid;
+    var deviceserverurl = this.serverUrl + 'classes/Device';
+    var str = '{"owner":{"__type":"Pointer","className":"_User","objectId":"wJ1XDdGTFf"}}';
+    var headers = {
+        "Content-Type": "Application/json",
+        "X-Parse-Application-Id": "MEDIA_PROM",
+        "Access-Control-Allow-Origin": "*",
+        "where" :  str  //JSON.stringify(requestobj)
+      }
+    if(userid != undefined){         
+        // var whereStr = JSON.stringify(requestobj);
+        // headers["where"] =whereStr;         
+    }
+    var req ={
+      method: "GET",
+      headers
+    }
+
+     console.log(req);
+    try {
+       var req_response = await fetch(deviceserverurl, {method: "GET", headers});
+       console.log(req_response);
+    } catch (error) {
+      console.log('Error1')
+    }
+
+      console.log(req_response);
+    try {
+       var userdata = await req_response.json();
+    } catch (error) {
+        console.log(error)
+    }
+    //console.log(userdata)
+    return userdata;
+  }
+
+
+
+
+   private async getVideosbyUser(userid: string) {
+    //where={"owner":{"__type":"Pointer","className":"_User","objectId":"wJ1XDdGTFf"}}
+     // userid = "wJ1XDdGTFf";
+    console.log("Videos request");
+    var requestobj = { "owner": { "__type": "Pointer", "className": "_User", "objectId": "wJ1XDdGTFf" }};
+    requestobj.owner.objectId = userid;
+    var deviceserverurl = this.serverUrl + 'classes/MediaItem';
+
+    var headers = {
+        "Content-Type": "Application/json",
+        "X-Parse-Application-Id": "MEDIA_PROM",
+      }
+    if(userid != undefined){         
+         var whereStr = JSON.stringify(requestobj);
+         headers["where"] =whereStr;         
+    }
+    var req ={
+      method: "GET",
+      headers
+    }
+
+    try {
+       var req_response = await fetch(deviceserverurl, {method: "GET", headers});
+    } catch (error) {
+      console.log(error)
+    }
+
+    try {
+       var userdata = await req_response.json();
+    } catch (error) {
+        console.log(error)
+    }
+    //console.log(userdata)
+    return userdata;
+  }
+
+ 
 }
